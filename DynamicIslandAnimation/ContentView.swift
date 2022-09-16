12366090e3749ead6f8779e9ae9949f8827ec842
Sarawut Saengchan <namosarawut02@gmail.com>
2022-09-16 22:25:49 +0700

    Initial Commit

---
+++ DynamicIslandAnimation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DynamicIslandAnimation
+//
+//  Created by Namo Sarawut on 16/9/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DynamicIslandAnimation/DynamicIslandAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIslandAnimationApp.swift
+//  DynamicIslandAnimation
+//
+//  Created by Namo Sarawut on 16/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct DynamicIslandAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
